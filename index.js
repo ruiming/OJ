78cbcf0e4a24f7198bccd34d67e82b1cbb0392c5
ruiming <ruiming.zhuang@gmail.com>
2016-12-08 16:16:17 +0800

    Use ESLint

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ async function run() {
         // Image: 已经 pull 的镜像
         // Binds: 挂载目录
         const container = await docker.createContainer({
-            Image: 'ruiming/gcc',
-            AttachStdin: true,
+            Image:        'ruiming/gcc',
+            AttachStdin:  true,
             AttachStdout: true,
             AttachStderr: true,
-            Tty: true,
-            Cmd: ['/bin/bash'],
-            Binds: ['/home/ruiming/Desktop:/cpp']
+            Tty:          true,
+            Cmd:          ['/bin/bash'],
+            Binds:        ['/home/ruiming/Desktop:/cpp']
         })
         container.start  = Promise.promisify(container.start)
         container.exec   = Promise.promisify(container.exec)
@@ -28,14 +28,14 @@ async function run() {
         // 启动容器, 执行命令
         await container.start()
         const exec = await container.exec({
-            Cmd: ["./cpp/main.o", "<", "./cpp/main.in"],
-            AttachStdin: true,
+            Cmd:          ['./cpp/main.o', '<', './cpp/main.in'],
+            AttachStdin:  true,
             AttachStdout: true
         })
         exec.start   = Promise.promisify(exec.start)
         exec.inspect = Promise.promisify(exec.inspect)
         const stream = await exec.start({
-            stdin: true,
+            stdin:  true,
             hijack: true
         })
         stream.setEncoding('utf8')
