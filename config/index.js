d44537495e0a12a166e257fde1be96e0adad5dea
ruiming <ruiming.zhuang@gmail.com>
2016-12-13 10:30:42 +0800

    update

--- config/index.js
+++ config/index.js
@@ -2,6 +2,9 @@ const fs = Promise.promisifyAll(require('fs-extra'))
 const path = require('path')
 
 module.exports = {
+    DIR: {
+        QUESTIONS: path.resolve(__dirname, '..', 'tests')
+    },
     OUTPUT: {
         CPP: path.resolve(__dirname, '..', 'temp')
     }
--- server/controllers/problemController.js
+++ server/controllers/problemController.js
@@ -6,5 +6,9 @@ const config = require('../../config')
 
 module.exports.check = async (ctx, next) => {
     let { problemId, code } = ctx.request.body
-    await fs.outputFileAsync(path.resolve(config.OUTPUT.CPP, `${problemId}.${Date.now()}.cpp`), code)
+    // 将用户代码和对应测试文件存入临时文件夹
+    await Promise.all([
+        fs.outputFileAsync(path.resolve(config.OUTPUT.CPP, `${problemId}.${Date.now()}.cpp`), code),
+        fs.copy(path.resolve(config.DIR.QUESTIONS, `${problemId}.in`), path.resolve(config.OUTPUT.CPP, `${problemId}.in`))
+    ])
 }
